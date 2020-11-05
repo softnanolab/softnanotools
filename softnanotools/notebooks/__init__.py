c477c181aa530f692bd5b9529b2d2b8154ecfc59
Debesh Mandal <debesh.mandal13@imperial.ac.uk>
2020-11-05 15:29:36 +0000

    tidied merge files

---
+++ softnanotools/notebooks/__init__.py
@@ -0,0 +1,4 @@
+from ._core import IPythonCell, IPythonNotebook
+from ._utils import IPythonTools
+
+merge = IPythonTools.merge
--- softnanotools/notebooks/_cli.py
+++
@@ -1,5 +0,0 @@
-import argparse
-from .merge import main as _main
-from .merge import _description, _parser
-args = _parser().parse_args()
-_main(**vars(args))
