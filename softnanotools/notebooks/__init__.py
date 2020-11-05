c83b7c53ced2545ef29c38c02f4e86a4a92ff4f8
Debesh Mandal <debesh.mandal13@imperial.ac.uk>
2020-11-05 18:17:44 +0000

    updated docstring of notebooks

--- softnanotools/notebooks/__init__.py
+++ softnanotools/notebooks/__init__.py
@@ -1,3 +1,23 @@
+"""
+The `notebooks` module contains tools for importing and
+scripting the generation of Jupyter Notebooks.
+
+To a computer scientist Jupyter Notebooks are nothing 
+but key:value objects that can be represented in a
+number of ways. For example the YAML file format 
+(which is a parent of JSON) can be used, or indeed its
+child JSON, or XML ... Okay, these are about it.
+
+In Python key:value objects are called dictionaries
+and Jupyter Notebooks are dictionaries with some 
+added rules, and so can be manipulated within Python.
+
+classes:
+    IPythonNotebook - notebook container
+    IPythonCell - element in notebook
+    IPythonTools - a class full of static methods
+
+"""
 from ._core import IPythonCell, IPythonNotebook
 from ._utils import IPythonTools
 
