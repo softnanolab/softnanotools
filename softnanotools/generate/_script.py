1a09aab46b424d9e82748a7227e33760d2f30b8b
Debesh Mandal <debesh.mandal@hotmail.com>
2021-09-29 01:10:56 +0100

    cli completed for generating a script

--- softnanotools/generate/_script.py
+++ softnanotools/generate/_script.py
@@ -5,15 +5,27 @@ from typing import Union
 from ..logger import Logger
 logger = Logger(__name__)
 
-def generate(fname: Union[Path, str], **kwargs):
+def generate(name: str, **kwargs):
+    if name.split('.')[-1] != 'py':
+        fname = Path(f'{name}.py')
+    else:
+        fname = Path(name)
+
+    _docstring = f"{name}.py - insert docstring here"
+
     with open(fname, 'w') as f:
-        f.write('#!/usr/bin/env python\n\n')
+        f.write('#!/usr/bin/env python\n')
+        f.write(f'"""{_docstring}"""\n')
         f.write('from softnanotools.logger import Logger\n')
         f.write('logger = Logger(__name__)\n\n')
         f.write('def main(**kwargs):\n')
+        f.write(f'    logger.info("Running {name}...")\n')
         f.write('    return\n\n')
         f.write("if __name__ == '__main__':\n")
         f.write('    import argparse\n')
-        f.write('    parser = argparse.ArgumentParser()\n')
+        f.write(
+            '    parser = argparse.ArgumentParser('
+            f'description="{name}.py")\n'
+        )
         f.write('    main(**vars(parser.parse_args()))\n')
     return
--- softnanotools/generate/cli.py
+++ softnanotools/generate/cli.py
@@ -3,14 +3,18 @@ import argparse
 from ..logger import Logger
 logger = Logger(__name__)
 
-def run(command: str, **kwargs):
+from . import _script
+
+def run(command: str, kind: str, name: str, **kwargs):
     assert command == 'generate'
-    logger.info(kwargs)
+    if kind == 'script':
+        _script.generate(name)
     return
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--name', default='test')
+    parser.add_argument('kind')
+    parser.add_argument('name')
     args = parser.parse_args()
-    run(**vars(args))
+    run('generate', **vars(args))
     return
