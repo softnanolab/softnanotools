1c82c0ffc08c430149f8a0b57dfa1898626d6ea5
Debesh Mandal <debesh.mandal@hotmail.com>
2021-12-06 17:16:58 +0000

    updated template for testing test genreation

---
+++ test/test_generate/test_tests.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""Tests softnanotools.generate._tests
+"""
+from pathlib import Path
+import softnanotools.generate._tests as tests
+
+TARGET = Path(__file__).parent / 'example.py'
+
+if __name__ == '__main__':
+    pass
